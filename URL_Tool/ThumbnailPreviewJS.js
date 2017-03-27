f372e94feddf28b4aa841064ae396aed72c143f8
Won Yong Ha <hpmsora@gmail.com>
2017-03-27 01:45:43 -0400

    27 March 2017

--- URL_Tool/ThumbnailPreviewJS.js
+++ URL_Tool/ThumbnailPreviewJS.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $.get('http://stackoverflow.com/questions/17712185/jquery-load-is-not-loading-a-local-html-file-inside-a-cocoa-webview', function (url) {
+  $.get('https://www.youtube.com/results?search_query=making+link+thumbnail', function (url) {
     $('.PreviewTitle').html($(url).find('meta[name=title]').attr("content"));
   });
 });
