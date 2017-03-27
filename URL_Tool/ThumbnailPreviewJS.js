7580d8bb3c5e8931401fa5cd4941a4b8b148170d
Won Yong Ha <hpmsora@gmail.com>
2017-03-27 12:46:48 -0400

    27 March 2017

--- URL_Tool/ThumbnailPreviewJS.js
+++ URL_Tool/ThumbnailPreviewJS.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $.get('https://www.youtube.com/results?search_query=making+link+thumbnail', function (url) {
-    $('.PreviewTitle').html($(url).find('meta[name=title]').attr("content"));
+    $('.PreviewTitle').html("Hello");
   });
 });
