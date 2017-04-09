852aced9b361e74d3d8c9a2a9f5765d1c5e9d102
Won Yong Ha <hpmsora@gmail.com>
2017-04-09 19:02:30 -0400

    9 April 2017

--- URL_Tool/ThumbnailPreviewJS.js
+++ URL_Tool/ThumbnailPreviewJS.js
@@ -8,7 +8,5 @@ var url = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(qu
     console.log(html.find("meta[name='twitter:description']").attr('content'));
 
     $('#PreviewTitle').html(html.find("meta[name='twitter:description']").attr('content') || 'no title found');
-
-    console.log(html.find('meta[name=twitter:title]').attr('content') || 'no keywords found');
   });
 console.log('-------------------');
