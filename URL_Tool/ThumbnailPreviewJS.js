c452062fe20674d9edf2febc210a5cf9c16b40bc
Won Yong Ha <hpmsora@gmail.com>
2017-04-09 18:45:36 -0400

    9 April 2017

--- URL_Tool/ThumbnailPreviewJS.js
+++ URL_Tool/ThumbnailPreviewJS.js
@@ -1,9 +1,9 @@
-var query = 'select * from html where url="' + 'http://www.naver.com/' + '" and xpath="*"';
+var query = 'select * from html where url="' + 'http://stackoverflow.com/questions/35911262/fetching-metadata-from-url' + '" and xpath="*"';
 var url = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query);
 
   $.get(url, function(data) {
     var html = $(data).find('html');
-    $('#PreviewTitle').html(html.find('meta[name=twitter:title]').attr('content') || 'no keywords found');
+    $('#PreviewTitle').html(html.find('meta[name=twitter:title]').attr('content') || 'no title found');
 
     console.log(html);
     console.log(html.find('meta[name=twitter:title]').attr('content') || 'no keywords found');
