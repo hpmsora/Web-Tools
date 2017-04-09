a5b22000ae33b7d299bb76b9d927a109afcb9ecd
Won Yong Ha <hpmsora@gmail.com>
2017-04-09 18:27:33 -0400

    9 April 2017

--- URL_Tool/ThumbnailPreviewJS.js
+++ URL_Tool/ThumbnailPreviewJS.js
@@ -1,7 +1,7 @@
-$("#PreviewTitle").load("{http://www.naver.com/} meta[name='Author']", function(response, status, xhr){
-    var $meta = $("meta", this);
-
-    $(this).text($meta.attr("content"));
-
-    $meta.remove();
+var scrape = require('html-metadata');
+ 
+var url = "http://blog.woorank.com/2013/04/dublin-core-metadata-for-seo-and-usability/";
+ 
+scrape(url).then(function(metadata){
+    console.log(metadata);
 });
