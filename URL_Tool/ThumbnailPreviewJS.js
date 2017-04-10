27c7816633d3e057a9ba4b48fa33b457fe1df9d7
Won Yong Ha <hpmsora@gmail.com>
2017-04-09 21:32:23 -0400

    9 April 2017

--- URL_Tool/ThumbnailPreviewJS.js
+++ URL_Tool/ThumbnailPreviewJS.js
@@ -1,6 +1,4 @@
 var query = 'select * from html where url="' + document.getElementById("MessageBox").value + '" and xpath="*"';
-
-console.log(query);
 var url = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query);
 
   $.get(url, function(data) {
@@ -9,6 +7,7 @@ var url = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(qu
     var title = html.find("meta[property='og:image']").attr('itemprop') || '';
     if(title == '')
       title = html.title;
+    console.log(title);
     $('.PreviewImage').html(title);
     $('.PreviewTitle').html(html.find("meta[name='twitter:title']").attr('content') || '');
     $('.PreviewDescription').html(html.find("meta[name='twitter:description']").attr('content') || '');
