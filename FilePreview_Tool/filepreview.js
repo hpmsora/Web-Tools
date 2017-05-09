e9d103553497736857baada22a3f0a745eadfa9c
Won Yong Ha <hpmsora@gmail.com>
2017-05-09 18:36:28 -0400

    May 9 2017 - psd check

--- FilePreview_Tool/filepreview.js
+++ FilePreview_Tool/filepreview.js
@@ -1,27 +1,5 @@
 var PSD = require('psd');
 
-$(document).ready(function(){
-
-  var $UploadedFile = $('#UploadedFile');
-
-  $UploadedFile.on('change', function (e) {
-
-    var UploadedFile = e.target.files[0];
-    var tempAddress = URL.UploadedFile;
-
-    console.log(UploadedFile.value);
-    console.log(tempAddress);
-  });
-})
-
-document.getElementById("UploadedFile").onchange = function () {
-    var reader = new FileReader();
-
-    reader.onload = function (e) {
-        // get loaded data and render thumbnail.
-        document.getElementById("image").src = e.target.result;
-    };
-
-    // read the image file as a data URL.
-    reader.readAsDataURL(this.files[0]);
-};
+PSD.fromURL("https://hpmsora.github.io/Web-Tools/FilePreview_Tool/Assets/samplePSD.psd").then(function(psd) {
+  document.getElementById('testIfram_Class').appendChild(psd.image.toPng());
+});
