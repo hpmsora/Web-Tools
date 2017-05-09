c1eb5912c15de1d67e62ca388123c12d20952426
Won Yong Ha <hpmsora@gmail.com>
2017-05-09 19:37:06 -0400

    May 9 2017 - pds css add

--- FilePreview_Tool/filepreview.js
+++ FilePreview_Tool/filepreview.js
@@ -3,11 +3,8 @@
 var PSD = require('psd');
 
 PSD.fromURL("https://hpmsora.github.io/Web-Tools/FilePreview_Tool/Assets/samplePSD.psd").then(function(psd) {
-  var psdImage = psd.image.toPng();
-  psdImage.style.max-width = "500px";
-  psdImage.style.max-height = "500px";
 
-  document.getElementById('testPSD').appendChild(psdImage);
+  document.getElementById('testPSD').appendChild(psd.image.toPng());
 });
 //------------------------------------------------
 
