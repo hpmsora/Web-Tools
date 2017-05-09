989e7d510a3d0e7bac3b2283c29a67d9f58cc472
Won Yong Ha <hpmsora@gmail.com>
2017-05-09 19:02:29 -0400

    May 9 2017 - psd check

--- FilePreview_Tool/filepreview.js
+++ FilePreview_Tool/filepreview.js
@@ -1,5 +1,7 @@
 var PSD = require('psd');
+var psd = PSD.fromFile("https://hpmsora.github.io/Web-Tools/FilePreview_Tool/Assets/samplePSD.psd");
+psd.parse();
 
-PSD.fromURL("https://hpmsora.github.io/Web-Tools/FilePreview_Tool/Assets/samplePSD.psd").then(function(psd) {
+/*PSD.fromURL("https://hpmsora.github.io/Web-Tools/FilePreview_Tool/Assets/samplePSD.psd").then(function(psd) {
   document.getElementById('testIfram_Class').appendChild(psd.image.toPng());
-});
+});*/
