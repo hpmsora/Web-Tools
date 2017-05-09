c9c11e8f9093bedccd22a42c27c36b926771ce13
Won Yong Ha <hpmsora@gmail.com>
2017-05-09 19:17:57 -0400

    May 9 2017 - pds css overflow change

--- FilePreview_Tool/filepreview.js
+++ FilePreview_Tool/filepreview.js
@@ -1,5 +1,5 @@
 var PSD = require('psd');
 
 PSD.fromURL("https://hpmsora.github.io/Web-Tools/FilePreview_Tool/Assets/samplePSD.psd").then(function(psd) {
-  document.getElementById('testPSD').appendChild(psd.image.toPng());
+  document.getElementById('testPSD').appendChild(psd.image.toPng().css({"width":"500px", "height":"500px"}));
 });
